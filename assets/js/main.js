7ca985a58eff5c666d8c794e1a372667af39b30c
Felipe Araujo <araujo.felipedev@gmail.com>
2022-07-13 15:27:32 -0300

    added curved sections JS switcher to all containers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 var x = window.matchMedia("(max-width: 768px)")
 const topSection1 = document.querySelector('[data-topSection]');
 const bottomSection1 = document.querySelector('[data-bottomSection]');
+const topSection2 = document.querySelector('[data-topSection2]');
+const bottomSection2 = document.querySelector('[data-bottomSection2]');
+const footerSection = document.querySelector('[data-footerSection]');
 
 curvedSectionsChange(x) // Call listener function at run time
 x.addListener(curvedSectionsChange) // Attach listener function on state changes
@@ -9,8 +12,14 @@ function curvedSectionsChange(x) {
     if (x.matches) { // If media query matches 
     topSection1.src = 'file:///C:/Users/felip/OneDrive/Documentos/Alura%20HTML%20&%20CSS/Pr%C3%A1tica/Challenge/huddle-landing-page-with-curved-sections-master/assets/images/bg-section-top-mobile-1.svg';
     bottomSection1.src = 'file:///C:/Users/felip/OneDrive/Documentos/Alura%20HTML%20&%20CSS/Pr%C3%A1tica/Challenge/huddle-landing-page-with-curved-sections-master/assets/images/bg-section-bottom-mobile-1.svg';
+    topSection2.src = 'file:///C:/Users/felip/OneDrive/Documentos/Alura%20HTML%20&%20CSS/Pr%C3%A1tica/Challenge/huddle-landing-page-with-curved-sections-master/assets/images/bg-section-top-mobile-2.svg';
+    bottomSection2.src = 'file:///C:/Users/felip/OneDrive/Documentos/Alura%20HTML%20&%20CSS/Pr%C3%A1tica/Challenge/huddle-landing-page-with-curved-sections-master/assets/images/bg-section-bottom-mobile-2.svg';
+    footerSection.src = 'file:///C:/Users/felip/OneDrive/Documentos/Alura%20HTML%20&%20CSS/Pr%C3%A1tica/Challenge/huddle-landing-page-with-curved-sections-master/assets/images/bg-footer-top-mobile.svg';
     } else {
         topSection1.src = 'file:///C:/Users/felip/OneDrive/Documentos/Alura%20HTML%20&%20CSS/Pr%C3%A1tica/Challenge/huddle-landing-page-with-curved-sections-master/assets/images/bg-section-top-desktop-1.svg';
         bottomSection1.src = 'file:///C:/Users/felip/OneDrive/Documentos/Alura%20HTML%20&%20CSS/Pr%C3%A1tica/Challenge/huddle-landing-page-with-curved-sections-master/assets/images/bg-section-bottom-desktop-1.svg';
+        topSection2.src = 'file:///C:/Users/felip/OneDrive/Documentos/Alura%20HTML%20&%20CSS/Pr%C3%A1tica/Challenge/huddle-landing-page-with-curved-sections-master/assets/images/bg-section-top-desktop-2.svg';
+        bottomSection2.src = 'file:///C:/Users/felip/OneDrive/Documentos/Alura%20HTML%20&%20CSS/Pr%C3%A1tica/Challenge/huddle-landing-page-with-curved-sections-master/assets/images/bg-section-bottom-desktop-2.svg';
+        footerSection.src = 'file:///C:/Users/felip/OneDrive/Documentos/Alura%20HTML%20&%20CSS/Pr%C3%A1tica/Challenge/huddle-landing-page-with-curved-sections-master/assets/images/bg-footer-top-desktop.svg';
     }
   }
